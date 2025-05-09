92d58fe50495936973a46d42bdc52c2b7b66535c
Diomuzan <94070451+TheCodeifier@users.noreply.github.com>
2025-05-09 19:02:04 +0200

    Created all classes

--- src/main.java
+++ src/main.java
@@ -1,3 +1,93 @@
-abstract class send {
-    
+import java.util.Scanner;
+
+abstract class MessageSender {
+    public final void verzend() {
+        connect();
+        sendMessage();
+        disconnect();
+    }
+
+    protected abstract void connect();
+    protected abstract void sendMessage();
+    protected abstract void disconnect();
+}
+
+class EmailSender extends MessageSender {
+    @Override
+    protected void connect() {
+        System.out.println("Email: Verbinden met mailserver...");
+    }
+
+    @Override
+    protected void sendMessage() {
+        System.out.println("Email: Bericht verzenden...");
+    }
+
+    @Override
+    protected void disconnect() {
+        System.out.println("Email: Verbinding verbreken.");
+    }
+}
+
+class SMSSender extends MessageSender {
+    @Override
+    protected void connect() {
+        System.out.println("SMS: Verbinden met SMS-gateway...");
+    }
+
+    @Override
+    protected void sendMessage() {
+        System.out.println("SMS: Bericht verzenden...");
+    }
+
+    @Override
+    protected void disconnect() {
+        System.out.println("SMS: Verbinding verbreken.");
+    }
+}
+
+class PushSender extends MessageSender {
+    @Override
+    protected void connect() {
+        System.out.println("Push: Verbinden met pushserver...");
+    }
+
+    @Override
+    protected void sendMessage() {
+        System.out.println("Push: Bericht verzenden...");
+    }
+
+    @Override
+    protected void disconnect() {
+        System.out.println("Push: Verbinding verbreken.");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kies een methode om het bericht te versturen: email, sms, of push");
+        String keuze = scanner.nextLine().toLowerCase();
+
+        MessageSender sender;
+
+        switch (keuze) {
+            case "email":
+                sender = new EmailSender();
+                break;
+            case "sms":
+                sender = new SMSSender();
+                break;
+            case "push":
+                sender = new PushSender();
+                break;
+            default:
+                System.out.println("Ongeldige keuze.");
+                scanner.close();
+                return;
+        }
+
+        sender.verzend();
+        scanner.close();
+    }
 }
